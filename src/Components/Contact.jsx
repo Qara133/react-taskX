3cc9c467992cc2bdc56e861e018bed4b52be1bdc
AkuLa770 <qara.qarali24@gmail.com>
2024-12-17 11:48:57 +0400

    first commit

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='main'>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum deserunt illum, illo iure pariatur, cupiditate sed maiores dicta quo repellendus beatae inventore, consequuntur suscipit possimus labore excepturi corporis voluptatum necessitatibus!
+    </div>
+  )
+}
+
+export default Contact
